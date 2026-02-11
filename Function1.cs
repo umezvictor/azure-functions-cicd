2e0bfb09e9b30787dd174ab2b4ef68e885207aea
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-02-11 13:50:35 +0200

    sdsd

--- Function1.cs
+++ Function1.cs
@@ -17,7 +17,6 @@ public class Function1
     [Function("Function1")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
